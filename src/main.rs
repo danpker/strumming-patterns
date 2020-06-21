744608b23af266c0f8da37029b0812155a3a8a0b
Daniel Parker <danpker@gmail.com>
2020-06-21 16:15:27 +0100

    Add beat number selection and easy mode

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,31 @@
+use clap::Clap;
 use rand::prelude::*;
 
+#[derive(Clap)]
+#[clap(name = "basic")]
+struct Opts {
+    /// The number of beats per bar.
+    #[clap(short, long, default_value = "4")]
+    beats: u8,
+    /// Ensure there is a down strum on the first beat.
+    #[clap(short, long)]
+    easy: bool,
+}
+
 fn main() {
-    println!("1 + 2 + 3 + 4 +");
+    let opts = Opts::parse();
+
+    let mut header = String::new();
+    for beat in 1..=opts.beats {
+        let beat_header = format!("{} + ", beat);
+        header.push_str(&beat_header);
+    }
+    println!("1 + 2 + 3 + 4 + ");
+
     let mut strums = String::new();
 
-    for _beat in 1..=4 {
-        if random() {
+    for beat in 1..=opts.beats {
+        if random() || (beat == 1 && opts.easy) {
             strums.push_str("D ");
         } else {
             strums.push_str("  ");
